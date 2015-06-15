551940c6cc64780c1e3a8f376bd0e30d72568930
glendc <decauwsemaecker.glen@gmail.com>
2015-06-15 14:52:12 +0000

    rendering mouse position

--- Trixel/WorkSpace.elm
+++ Trixel/WorkSpace.elm
@@ -35,14 +35,14 @@ renderTriangle: Float -> Float -> Float -> Float -> Float -> Float -> Shape
 renderTriangle x y x' y' x'' y'' =
   polygon [ (x, y), (x', y'), (x'', y'') ]
 
-renderTrixel: TrixelOrientation -> Float -> Float -> Float -> Form
-renderTrixel orientation x y size =
+renderTrixel: TrixelOrientation -> Float -> Float -> Float -> (Shape -> Form) -> Form
+renderTrixel orientation x y size styleFunction =
   let trixel = case orientation of
     Up -> renderTriangle x y (x + (size / 2)) (y + size) (x + size) y
     Down -> renderTriangle x (y + size) (x + (size / 2)) y (x + size) (y + size)
     Left -> renderTriangle x y x (y + size) (x + size) (y + (size / 2))
     Right -> renderTriangle x (y + (size / 2)) (x + size) (y + size) (x + size) y
-  in outlined (solid grey) trixel
+  in styleFunction trixel
 
 getTrixelOrientation: Int -> Int -> TrixelMode -> TrixelOrientation
 getTrixelOrientation x y mode =
@@ -63,14 +63,42 @@ renderTrixelRow state cx cy size w h trixels =
         x = (getCoordinateFromIndex cx xs w) + (offset.x * state.trixelInfo.scale)
         y = (getCoordinateFromIndex cy ys h) + (offset.y * state.trixelInfo.scale)
     in
-      (renderTrixel (getTrixelOrientation cx cy state.trixelInfo.mode) x y size) :: trixels
+      (renderTrixel (getTrixelOrientation cx cy state.trixelInfo.mode) x y size
+        (\s -> outlined (solid lightGrey) s)) :: trixels
         |> renderTrixelRow state (cx - 1) cy size w h
+
+---
+
+renderTrixelOnPosition: State -> FloatVec2D -> Float -> Float -> Form
+renderTrixelOnPosition state pos w h =
+  let hs = state.trixelInfo.size / 2
+      cx = round ((pos.x) / (state.trixelInfo.size / 2))
+      cy = round ((h - pos.y + hs) / state.trixelInfo.size)
+      
+      (xs, ys) = getSizePairFromTrixelMode state.trixelInfo.size state.trixelInfo.mode
+      x = (getCoordinateFromIndex cx xs w) + (state.trixelInfo.offset.x * state.trixelInfo.scale)
+      y = (getCoordinateFromIndex cy ys h) + (state.trixelInfo.offset.y * state.trixelInfo.scale)
+  in
+    renderTrixel
+      (getTrixelOrientation cx cy state.trixelInfo.mode)
+      x y state.trixelInfo.size (\s -> filled red s)
+
+
+renderMouse: State -> Float -> Float -> List Form -> List Form
+renderMouse state w h trixels =
+  case state.mouseState of
+    MouseNone -> trixels
+    MouseHover pos -> (renderTrixelOnPosition state pos w h) :: trixels
+
+---
 
 renderWorkSpace: State -> Int -> Int -> Float -> Float -> Float -> List Form -> List Form
 renderWorkSpace state cx cy size w h trixels =
-  if cy == 0 then trixels else
-    renderTrixelRow state cx cy size w h trixels
-      |> renderWorkSpace state cx (cy - 1) size w h
+  if cy == 0
+    then renderMouse state w h trixels
+    else
+      renderTrixelRow state cx cy size w h trixels
+        |> renderWorkSpace state cx (cy - 1) size w h
 
 ---
 
