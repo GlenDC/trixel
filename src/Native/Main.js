9a3a311031c4cfba4081553d380cfaed1b64fe88
glendc <decauwsemaecker.glen@gmail.com>
2015-07-05 22:49:53 +0000

    starting from a fresh codebase, will populate it based on new design reusing most of the old code

---
+++ src/Native/Main.js
@@ -0,0 +1,13 @@
+var nativeTrixel = {
+
+Main: function() {
+  var trixelEditor = Elm.fullscreen(Elm.Trixel.Main, {
+    //setWindowDimensions: { x: 0, y: 0 },
+  });
+  
+  //trixelEditor.ports.setWindowDimensions.send(
+  //    {x: window.innerWidth, y: window.innerHeight}
+  //);
+},
+
+};
