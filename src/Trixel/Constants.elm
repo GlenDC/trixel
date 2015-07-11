c7d1c2bc064d973c22866992d5ed76ee68b13e89
glendc <decauwsemaecker.glen@gmail.com>
2015-07-11 21:17:27 +0000

    re-adding global constants

---
+++ src/Trixel/Constants.elm
@@ -0,0 +1,31 @@
+module Trixel.Constants where
+
+
+version : String
+version =
+  "0.1.12"
+
+
+githubRepositoryURL : String
+githubRepositoryURL =
+  "https://github.com/GlenDC/trixel"
+
+
+newsletterSubscribeURL : String
+newsletterSubscribeURL =
+  "http://eepurl.com/brwmSn"
+
+
+editorURL : String
+editorURL =
+  "http://trixel.it"
+
+
+email : String
+email =
+  "contact@glendc.com"
+
+
+sqrt3 : Float
+sqrt3 =
+  sqrt 3
